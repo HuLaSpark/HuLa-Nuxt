7310bb32fc127c036eba56a8609c38187aaf1774
Dawn <2439646234@qq.com>
2024-12-18 22:54:13 +0800

    ci(config): 🎡 更新hula版本 (#31)
    * fix(component): :bug: 修复因版本号使用gitee获取导致的问题
    
    * ci(config): :ferris_wheel: 更新hula版本

--- components/common/Header.vue
+++ components/common/Header.vue
@@ -10,11 +10,11 @@
           </label>
         </NuxtLink>
 
-        <div v-if="hulaVersion" class="relative inline-flex">
-          <UTooltip :text="`最新版本: ${hulaVersion}`">
+        <div class="relative inline-flex">
+          <UTooltip :text="`最新版本: ${config.MasterVersion}`">
             <span
               class="inline-flex items-center cursor-pointer text-xs px-1.5 py-0.5 bg-teal-50 dark:bg-teal-400 dark:bg-opacity-10 text-teal-500 dark:text-teal-400 ring-1 ring-inset ring-teal-500 dark:ring-teal-400 ring-opacity-25 dark:ring-opacity-25 -mb-[2px] rounded font-semibold">
-              {{ hulaVersion }}
+              {{ config.MasterVersion }}
             </span>
           </UTooltip>
         </div>
@@ -108,9 +108,8 @@
           <span class="text-lg text-teal-900 dark:text-teal-600 font-semibold">HuLa</span>
         </NuxtLink>
         <span
-          v-if="hulaVersion"
           class="inline-flex items-center text-xs px-1.5 py-0.5 bg-teal-50 dark:bg-teal-400 dark:bg-opacity-10 text-teal-500 dark:text-teal-400 ring-1 ring-inset ring-teal-500 dark:ring-teal-400 ring-opacity-25 dark:ring-opacity-25 rounded font-semibold">
-          {{ hulaVersion }}
+          {{ config.MasterVersion }}
         </span>
       </div>
 
@@ -200,7 +199,7 @@
 </template>
 
 <script setup lang="ts">
-const hulaVersion = ref()
+const config = useAppConfig()
 const route = useRoute()
 const isOpen = ref(false)
 const panelVisible = ref(false)
@@ -327,10 +326,6 @@ const handleMobileAction = (action: string) => {
       break
   }
 }
-
-onMounted(() => {
-  hulaVersion.value = localStorage.getItem('hulaVersion') || void 0
-})
 </script>
 
 <style scoped lang="scss"></style>
