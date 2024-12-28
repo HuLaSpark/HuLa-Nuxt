4dd4cea78e0fa08241ec166628b9a0eae9a4a46b
Dawn <2439646234@qq.com>
2024-12-28 16:24:01 +0800

    fix(style): 🐛 修改页面样式问题 (#36)
    * fix(style): :bug: 修改页面布局中内容滚动问题
    
    * fix(style): :bug: 修改页面样式问题
    
    ---------
    
    Co-authored-by: yaoxingpu <yaoxpu@163.com>

--- components/common/Header.vue
+++ components/common/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="bg-background/75 backdrop-blur border-b border-gray-200 dark:border-gray-800 -mb-px sticky top-0 z-50 lg:mb-0 lg:border-0">
+    class="bg-background/75 backdrop-blur border-b border-gray-200 dark:border-gray-800 -mb-px sticky top-0 z-50 lg:mb-0">
     <div class="mx-auto px-4 sm:px-6 lg:px-8 max-w-7xl flex justify-between items-center gap-3 h-[--header-height]">
       <div class="lg:flex-1 flex items-center gap-2.5">
         <NuxtLink to="/">
